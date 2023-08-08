9977d1bde1c18e27bb5f03f069f0d17280e50b60
Andres Velez <frozenflameswh@gmail.com>
2023-08-07 19:26:39 -0500

    Crear compras
    Todo funcional, menos crear insumos

---
+++ PF_Pach_OS/Controllers/DetallesComprasController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PF_Pach_OS.Models;
+
+namespace PF_Pach_OS.Controllers
+{
+    public class DetallesComprasController : Controller
+    {
+        private readonly Pach_OSContext _context;
+
+        public DetallesComprasController(Pach_OSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DetallesCompras
+        public async Task<IActionResult> Index()
+        {
+            var pach_OSContext = _context.DetallesCompras.Include(d => d.IdCompraNavigation).Include(d => d.IdInsumoNavigation);
+            return View(await pach_OSContext.ToListAsync());
+        }
+
+        [HttpPost]
+        public IActionResult GuardarCompra(List<DetallesCompra> compras)
+        {
+            foreach (var compra in compras)
+            {
+                DetallesCompra detalle = new();
+
+                int? nombreInsumo = compra.IdInsumo;
+                int? cantidad = compra.Cantidad;
+
+                TempData["Mensaje"] = $"Productos agregados exitosamente {nombreInsumo} {cantidad}";
+            }
+
+            // Puedes retornar un JSON como respuesta si es necesario
+            return NotFound();
+        }
+
+        // GET: DetallesCompras/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.DetallesCompras == null)
+            {
+                return NotFound();
+            }
+
+            var detallesCompra = await _context.DetallesCompras
+                .Include(d => d.IdCompraNavigation)
+                .Include(d => d.IdInsumoNavigation)
+                .FirstOrDefaultAsync(m => m.IdDetallesCompra == id);
+            if (detallesCompra == null)
+            {
+                return NotFound();
+            }
+
+            return View(detallesCompra);
+        }
+
+        // GET: DetallesCompras/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCompra"] = new SelectList(_context.Compras, "IdCompra", "IdCompra");
+            ViewData["IdInsumo"] = new SelectList(_context.Insumos, "IdInsumo", "IdInsumo");
+            return View();
+        }
+
+        // POST: DetallesCompras/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdDetallesCompra,PrecioInsumo,Cantidad,IdCompra,IdInsumo")] DetallesCompra detallesCompra)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(detallesCompra);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Crear","Compras", new {detallesCompra.IdCompra});
+            }
+            ViewData["IdCompra"] = new SelectList(_context.Compras, "IdCompra", "IdCompra", detallesCompra.IdCompra);
+            ViewData["IdInsumo"] = new SelectList(_context.Insumos, "IdInsumo", "IdInsumo", detallesCompra.IdInsumo);
+            return View(detallesCompra);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var detallesCompra = await _context.DetallesCompras.FindAsync(id);
+            _context.DetallesCompras.Remove(detallesCompra);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Crear", "Compras", new { detallesCompra.IdCompra });
+        }
+
+
+        private bool DetallesCompraExists(int id)
+        {
+          return (_context.DetallesCompras?.Any(e => e.IdDetallesCompra == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ PF_Pach_OS/Controllers/InsumoesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PF_Pach_OS.Models;
+
+namespace PF_Pach_OS.Controllers
+{
+    public class InsumoesController : Controller
+    {
+        private readonly Pach_OSContext _context;
+
+        public InsumoesController(Pach_OSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Insumoes
+        public async Task<IActionResult> Index()
+        {
+              return _context.Insumos != null ? 
+                          View(await _context.Insumos.ToListAsync()) :
+                          Problem("Entity set 'Pach_OSContext.Insumos'  is null.");
+        }
+
+        // GET: Insumoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Insumos == null)
+            {
+                return NotFound();
+            }
+
+            var insumo = await _context.Insumos
+                .FirstOrDefaultAsync(m => m.IdInsumo == id);
+            if (insumo == null)
+            {
+                return NotFound();
+            }
+
+            return View(insumo);
+        }
+
+        // GET: Insumoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insumoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdInsumo,NomInsumo,CantInsumo,Medida")] Insumo insumo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(insumo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(insumo);
+        }
+
+        // GET: Insumoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Insumos == null)
+            {
+                return NotFound();
+            }
+
+            var insumo = await _context.Insumos.FindAsync(id);
+            if (insumo == null)
+            {
+                return NotFound();
+            }
+            return View(insumo);
+        }
+
+        // POST: Insumoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdInsumo,NomInsumo,CantInsumo,Medida")] Insumo insumo)
+        {
+            if (id != insumo.IdInsumo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(insumo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InsumoExists(insumo.IdInsumo))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(insumo);
+        }
+
+        // GET: Insumoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Insumos == null)
+            {
+                return NotFound();
+            }
+
+            var insumo = await _context.Insumos
+                .FirstOrDefaultAsync(m => m.IdInsumo == id);
+            if (insumo == null)
+            {
+                return NotFound();
+            }
+
+            return View(insumo);
+        }
+
+        // POST: Insumoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Insumos == null)
+            {
+                return Problem("Entity set 'Pach_OSContext.Insumos'  is null.");
+            }
+            var insumo = await _context.Insumos.FindAsync(id);
+            if (insumo != null)
+            {
+                _context.Insumos.Remove(insumo);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InsumoExists(int id)
+        {
+          return (_context.Insumos?.Any(e => e.IdInsumo == id)).GetValueOrDefault();
+        }
+    }
+}
