3b85bbb1311a62ef9c0a9827abf068690f37b3e0
Andres Velez <frozenflameswh@gmail.com>
2023-08-14 14:29:22 -0500

    Compras - Falta agragar insumos

--- PF_Pach_OS/Models/DetallesCompra.cs
+++ PF_Pach_OS/Models/DetallesCompra.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace PF_Pach_OS.Models
 {
     public partial class DetallesCompra
     {
+        
         public int IdDetallesCompra { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public int? PrecioInsumo { get; set; }
-        public byte? Cantidad { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public int? Cantidad { get; set; }
         public int? IdCompra { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string? Medida { get; set; }
         public int? IdInsumo { get; set; }
 
         public virtual Compra? IdCompraNavigation { get; set; }
