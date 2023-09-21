cfd26c1ecf76abc0d2f4cbef2de4d80bfb615aae
Parsival1 <139830932+Parsival1@users.noreply.github.com>
2023-09-21 11:50:27 -0500

    Add files via upload

---
+++ Compra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PF_Pach_OS.Models
+{
+    public partial class Compra
+    {
+        public Compra()
+        {
+            DetallesCompras = new HashSet<DetallesCompra>();
+        }
+
+        public int IdCompra { get; set; }
+        public DateTime? FechaCompra { get; set; }
+        public int? Total { get; set; }
+        public int? IdEmpleado { get; set; }
+        public int? IdProveedor { get; set; }
+
+        public virtual Proveedore? IdProveedorNavigation { get; set; }
+        public virtual ICollection<DetallesCompra> DetallesCompras { get; set; }
+    }
+}
